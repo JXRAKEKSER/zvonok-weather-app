bef82914f4a85185bd90b51c4fe92324eb1c33c8
dmitrii <gorbatenko-2000@bk.ru>
2025-03-24 21:59:31 +0300

    Updates

---
+++ assets/useConditionLabel-MWR75QXD.js
@@ -0,0 +1,1 @@
+import{b as t}from"./forecast.service-CWKppyGn.js";import{h as n}from"./index-fRIpYGtX.js";function a(o){return{conditionLabel:n(()=>t.CONDTION_TO_LABELS[o.value]??"Неизвестно")}}export{a as u};
