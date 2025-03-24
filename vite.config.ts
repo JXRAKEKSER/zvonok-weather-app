38afc94488345c9df617e39b6a6d96fdb11f462b
dmitrii <gorbatenko-2000@bk.ru>
2025-03-24 22:18:35 +0300

    fix: исправить publicPath для gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/jxrakekser',
+  base: '/zvonok-weather-app',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
