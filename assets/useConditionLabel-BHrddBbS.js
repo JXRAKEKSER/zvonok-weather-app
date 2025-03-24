230a9873f5f0618d3472d577d21c83f06dd4f8e1
dmitrii <gorbatenko-2000@bk.ru>
2025-03-24 22:18:54 +0300

    Updates

---
+++ assets/useConditionLabel-BHrddBbS.js
@@ -0,0 +1,1 @@
+import{b as t}from"./forecast.service-BhWh6OVF.js";import{h as n}from"./index-6qLknGzp.js";function a(o){return{conditionLabel:n(()=>t.CONDTION_TO_LABELS[o.value]??"Неизвестно")}}export{a as u};
--- assets/useConditionLabel-nCMXpI-s.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./forecast.service-D9adIO76.js";import{h as n}from"./index-DPAigB_1.js";function a(o){return{conditionLabel:n(()=>t.CONDTION_TO_LABELS[o.value]??"Неизвестно")}}export{a as u};
